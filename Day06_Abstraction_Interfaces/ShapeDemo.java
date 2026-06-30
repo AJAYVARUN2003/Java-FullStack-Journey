09a6c89741c356dcdf2c75c02561e03c1fe6f788
AJAYVARUN2003 <ajayvarun2003@gmail.com>
2026-06-30 18:40:21 +0530

    Add files via upload

---
+++ Day06_Abstraction_Interfaces/ShapeDemo.java
@@ -0,0 +1,18 @@
+abstract class Shape{
+
+abstract void draw();
+}
+
+class Circle extends Shape{
+    @Override
+    void draw(){
+        System.out.println("Drawing Circle");
+    }
+}
+
+public class ShapeDemo{
+    public static void main(String[] args){
+       Shape s=new Circle();
+        s.draw();
+    }
+}
